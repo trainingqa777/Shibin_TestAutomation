bd2f07c90d1e310b338c534d0f248f2b19d9cb61
Pamy Ann Patrick <pamyannpatrick@gmail.com>
2023-01-16 19:23:33 -0500

    New class added

--- ShibinsTraining/src/basics/Location.java
+++ ShibinsTraining/src/basics/Location.java
@@ -5,7 +5,7 @@ public class Location {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String txt = "please locate the 'locate' in the sentence! ";
-		System.out.println("it's the location no "+txt.indexOf("c"));
+		System.out.println("it's the location no "+txt.indexOf("z"));
 	}
 
 }
